4e289b1bf47da5cb9b3231bf37a5186cd98694fd
Clarence Sarmiento <clarencesarmiento1020@gmail.com>
2023-08-23 00:03:18 +0800

    Updates

--- main.py
+++ main.py
@@ -12,28 +12,48 @@ import sys
 
 
 def main():
-    orders = {}
-    table = tabulate(get_menu(), headers='keys', tablefmt='pretty')
+    global ordered
+    options = [['SHOW', 'menu | order'], ['ORDERS', 'add | change | remove']]
+    print(tabulate(options, headers=['FUNCTIONS', 'OPTIONS'], tablefmt='pretty'))
     while True:
         try:
-            code, size, quantity = input("Enter Code, Size, and Quantity: ").upper().split(" ")
-            flavor, price = get_order(code, size, int(quantity))
-            if flavor not in orders:
-                orders[flavor] = price
-            else:
-                orders[flavor] += price
+            function, option = input('Command: ').split(' ')
+            function = function.upper()
+            option = option.lower()
+            if function == 'SHOW':
+                if option == 'menu':
+                    show_menu()
+                elif option == 'order':
+                    show_order()
+                else:
+                    raise ValueError
+            elif function == 'ORDERS':
+                orders = []
+                if option == 'add':
+                    while True:
+                        try:
+                            code, size, quantity = input("Enter Code, Size, and Quantity: ").upper().split(" ")
+                            orders.append(get_order(code, size, int(quantity)))
+                        except EOFError:
+                            add_order(orders)
+                            break
+                elif option == 'change':
+                    ...
+                elif option == 'remove':
+                    ...
+                else:
+                    raise ValueError
+        except ValueError:
+            print('Wrong Commands, check your functions or options.')
         except EOFError:
-            for order in orders:
-                print(order)
-            break
-
+            sys.exit()
 
 
 def get_menu():
     menu = []
     try:
-        with open('menu.csv') as csvfile:
-            reader = csv.DictReader(csvfile)
+        with open('menu.csv') as menufile:
+            reader = csv.DictReader(menufile)
             for row in reader:
                 menu.append(row)
         return menu
@@ -41,17 +61,33 @@ def get_menu():
         sys.exit('CSV file does not exist.')
 
 
+def show_menu():
+    print(tabulate(get_menu(), headers='keys', tablefmt='pretty'))
+
+
+def show_order():
+    ...
+...
+
+
+def add_order(order_list):
+    with open('order.csv', 'w', newline='') as orderfile:
+        fieldnames = ['QUANTITY', 'SIZE', 'FLAVOR', 'CODE', 'PRICE', 'TOTAL PRICE']
+        writer = csv.DictWriter(orderfile, fieldnames)
+        writer.writeheader()
+        for order in order_list:
+            writer.writerow(order)
+
+
+#
 def get_order(code: str, size: str, quantity: int):
-    sizes = ['SMALL', 'MEDIUM', 'LARGE']
+    sizes = ['REG', 'MD', 'LRG']
     for item in get_menu():
         if item['CODE'] == code and size in sizes:
             flavor = item['FLAVOR']
-            price = int(item[size]) * quantity
-            return flavor, price
-
-
-
-
+            price = float(item[size]) * int(quantity)
+            return {'QUANTITY': quantity, 'SIZE': size, 'FLAVOR': flavor, 'CODE': code, 'PRICE': item[size],
+                 'TOTAL PRICE': price}
 
 
 if __name__ == "__main__":
